ed9ba8d2fd41e95c4f5cfa59259dde06fd3dbaed
PizzaSlice-cmd <pizzaandpasta29@gmail.com>
2025-02-01 07:19:09 +0800

    Update hunyuan.sh

--- hunyuan.sh
+++ hunyuan.sh
@@ -191,6 +191,7 @@ function provisioning_get_pip_packages() {
     if [[ -n $PIP_PACKAGES ]]; then
             pip_install ${PIP_PACKAGES[@]}
     fi
+    pip_install hf_transfer  # Explicitly installing hf_transfer
 }
 
 function provisioning_get_nodes() {
