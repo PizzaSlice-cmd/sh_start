59402c1d7ec3266a6545442fd27da6ef9911f1c2
PizzaSlice-cmd <pizzaandpasta29@gmail.com>
2025-01-31 12:24:23 +0800

    Update hunyuan.sh

--- hunyuan.sh
+++ hunyuan.sh
@@ -25,7 +25,7 @@ NODES=(
     #"https://github.com/WASasquatch/was-node-suite-comfyui"
     #"https://github.com/WASasquatch/WAS_Extras"
     #"https://github.com/lilly1987/ComfyUI_node_Lilly"
-    "https://github.com/city96/ComfyUI-GGUF"
+    #"https://github.com/city96/ComfyUI-GGUF"
     #"https://github.com/pythongosssss/ComfyUI-Custom-Scripts"
     #"https://github.com/TinyTerra/ComfyUI_tinyterraNodes"
     #"https://github.com/ssitu/ComfyUI_UltimateSDUpscale"
